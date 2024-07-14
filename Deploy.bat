b946de46d5fa8af85fa703a31f54ebf77510c1e9
桜与白 <318099184@qq.com>
2024-07-15 06:31:02 +0800

    改进安装脚本

--- Deploy.bat
+++ Deploy.bat
@@ -1,2 +1,12 @@
 @ECHO OFF
 pip install -r requirements.txt
+pip install --upgrade tortoise-orm
+pip install --upgrade aiosqlite
+pip install --upgrade black
+pip install --upgrade iso8601
+pip install --upgrade nonebot-plugin-apscheduler
+pip install --upgrade nonebot-plugin-localstore
+pip install --upgrade numpy
+pip install "pydantic<2.0.0"
+pip list --outdated
+pause
