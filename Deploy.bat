f82f0f75a5806933c58be3d59f0c72376c53ab64
桜与白 <318099184@qq.com>
2024-07-19 03:35:47 +0800

    部署脚本改进，防止冲突导致的拒绝执行

--- Deploy.bat
+++ Deploy.bat
@@ -1,12 +1,27 @@
 @ECHO OFF
-pip install -r requirements.txt
-pip install --upgrade --no-deps aiosqlite
-pip install --upgrade --no-deps black
-pip install --upgrade --no-deps iso8601
-pip install --upgrade --no-deps nonebot-plugin-apscheduler
-pip install --upgrade --no-deps nonebot-plugin-localstore
-pip install --upgrade --no-deps numpy
-pip install --upgrade --no-deps tortoise-orm
+pip install --upgrade nonebot2[fastapi]
+pip install --upgrade nonebot-adapter-onebot
+
+pip install --upgrade nonebot-plugin-analysis-bilibili
+pip install --upgrade nonebot-plugin-emojimix
+pip install --upgrade nonebot-plugin-fishing
+pip install --upgrade nonebot-plugin-learning-chat
+pip install --upgrade nonebot-plugin-memes
+pip install --upgrade nonebot-plugin-multincm
+pip install --upgrade nonebot-plugin-treehelp
+pip install --upgrade nonebot-plugin-who-at-me
+pip install --upgrade nonebot-plugin-withdraw
+
+pip install --upgrade nonebot-plugin-apscheduler
+pip install --upgrade nonebot-plugin-localstore
+pip install --upgrade tortoise-orm
+
+pip install --upgrade aiocache
+pip install --upgrade aiosqlite
+pip install --upgrade black
+pip install --upgrade iso8601
+pip install --upgrade numpy
+
 pip install "pydantic<2.0.0"
 pip install "ruamel.yaml<0.18.0"
 pip list --outdated
