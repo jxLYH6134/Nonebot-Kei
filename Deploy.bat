d79d513bdb43687bd9a6ed7a99e9b02b6a10962a
桜与白 <318099184@qq.com>
2024-07-19 01:31:10 +0800

    部署脚本改进

--- Deploy.bat
+++ Deploy.bat
@@ -1,12 +1,13 @@
 @ECHO OFF
 pip install -r requirements.txt
-pip install --upgrade tortoise-orm
-pip install --upgrade aiosqlite
-pip install --upgrade black
-pip install --upgrade iso8601
-pip install --upgrade nonebot-plugin-apscheduler
-pip install --upgrade nonebot-plugin-localstore
-pip install --upgrade numpy
+pip install --upgrade --no-deps aiosqlite
+pip install --upgrade --no-deps black
+pip install --upgrade --no-deps iso8601
+pip install --upgrade --no-deps nonebot-plugin-apscheduler
+pip install --upgrade --no-deps nonebot-plugin-localstore
+pip install --upgrade --no-deps numpy
+pip install --upgrade --no-deps tortoise-orm
 pip install "pydantic<2.0.0"
+pip install "ruamel.yaml<0.18.0"
 pip list --outdated
 pause
--- Run.bat
+++ Run.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
 nb run
+pause
