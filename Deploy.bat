5f941ea3bb5264a03be9c5a8b1d412168486abfe
桜与白 <318099184@qq.com>
2024-07-21 02:50:06 +0800

    引用插件

--- Deploy.bat
+++ Deploy.bat
@@ -3,8 +3,11 @@ pip install --upgrade nonebot2[fastapi]
 pip install --upgrade nonebot2[httpx]
 pip install --upgrade nonebot-adapter-onebot
 
+pip install --upgrade nonebot-plugin-alias
 pip install --upgrade nonebot-plugin-analysis-bilibili
+pip install --upgrade nonebot-plugin-blacklist
 pip install --upgrade nonebot-plugin-emojimix
+pip install --upgrade nonebot-plugin-fakemsg
 pip install --upgrade nonebot-plugin-fishing
 pip install --upgrade nonebot-plugin-learning-chat
 pip install --upgrade nonebot-plugin-memes
@@ -12,6 +15,7 @@ pip install --upgrade nonebot-plugin-multincm
 pip install --upgrade nonebot-plugin-treehelp
 pip install --upgrade nonebot-plugin-who-at-me
 pip install --upgrade nonebot-plugin-withdraw
+pip install --upgrade nonebot_plugin-questionmark
 
 pip install --upgrade nonebot-plugin-apscheduler
 pip install --upgrade nonebot-plugin-localstore
